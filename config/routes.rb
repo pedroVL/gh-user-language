692fc76bbff006a5df338af093bf594ad2530fdf
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-12-17 12:26:37 +0000

    bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'searches#search'
+  get 'search', to: 'searches#search', as: :search
+  get 'result', to: 'searches#result', as: :result
 end
