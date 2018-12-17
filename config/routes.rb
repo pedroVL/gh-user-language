a337a4e463e5233fc81138c7a747b207cb322d49
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-12-17 11:22:35 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'searches#search'
+  get 'search', to: 'searches#search', as: :search
+  get 'result', to: 'searches#result', as: :result
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
